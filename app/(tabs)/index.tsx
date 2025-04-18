599a23f2adfa9490ccee0ee98a16edf7f0de53b8
LuluAmalia <luluamalia0221@gmail.com>
2025-04-18 21:50:50 +0700

    Android

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,101 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { View, StyleSheet, ScrollView, FlatList, Image, Text } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+export default function Home () {
+    const data = [
+        {
+            name: "Lulu Cantik",
+            age: 20,
+            city: "Jepang",
+        },
+        {
+            name: "Somad",
+            age: 16,
+            city: "Korea",
+        },
+        {
+            name: "Naufal",
+            age: 13,
+            city: "China",
+        },
+        {
+            name: "Ramadhan",
+            age: 5,
+            city: "Jepang",
+        },
+        {
+            name: "Andre Ardiawan",
+            age: 18,
+            city: "Singapura",
+        },
+    ];
+    const renderItem = ({item}) => {
+        return (
+            <View style={style.container2}>
+                <Text style={style.text1}>{item.name}</Text>
+                <Text style={style.text2}>{item.age}</Text>
+                <Text style={style.text3}>{item.city}</Text>
+            </View>
+        );
+    };
+    return (
+         <View style={style.container}>
+            <ScrollView>
+                <View>
+                    <Image
+                        source={{uri:"https://reactnative.dev/img/tiny_logo.png"}}
+                        style={style.image}
+                        resizeMode="contain"
+                        />
+                    <FlatList
+                        data={data}
+                        renderItem={renderItem}
+                        keyExtractor={item => item.name}
+                        />                    
+                </View>
+            </ScrollView>
+        </View>
+    );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+const style = StyleSheet.create({
+    image: {
+        width: 50,
+        height: 50,
+        borderWidth: 3,
+        borderColor: '#e8f5e9',
+        borderRadius:10,
+        padding:5,
+    },      
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor: "#32485C"
+    },
+    text1:{
+        fontSize : 20,
+        fontWeight : 'bold',
+        color : 'green',
+    },
+    text2:{
+        fontSize : 20,
+        fontWeight : '400',
+        color : 'black',
+    },
+    text3:{
+        fontSize : 20,
+        fontWeight :'bold',
+        color : 'black',
+    },
+    container2: {
+        justifyContent: "center",
+        alignItems: "center",
+        borderWidth: 1,
+        borderColor: "#EABCAC",  
+        width: 300,
+        borderRadius: 30,
+        padding: 5,
+        margin: 20,
+        backgroundColor: "#E2B091",
+    }
+    
 });
